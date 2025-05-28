5942d6971d961a7226e9e425d251a02dc6382231
Andres Fernandez <andres94.dev@gmail.com>
2025-05-28 12:40:02 +0200

    Actualizar versión del plugin maven-resources a 3.3.1 y comentar método de prueba

--- consum-scraping/src/test/java/site/amf_dev/consum/ConsumScrappingApplicationTests.java
+++ consum-scraping/src/test/java/site/amf_dev/consum/ConsumScrappingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConsumScrapingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
