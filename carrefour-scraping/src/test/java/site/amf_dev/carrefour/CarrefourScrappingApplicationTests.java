e38ea655da3d782f9cdd051e4cb7f3f31b2fdfaa
Andres Fernandez <andres94.dev@gmail.com>
2025-05-28 12:39:40 +0200

    Actualizar versión del plugin maven-resources y limpiar comentarios en pruebas

--- carrefour-scraping/src/test/java/site/amf_dev/carrefour/CarrefourScrappingApplicationTests.java
+++ carrefour-scraping/src/test/java/site/amf_dev/carrefour/CarrefourScrappingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CarrefourScrapingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
